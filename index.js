7b675cf4967193b9119c8f3f202d669d132a2127
Rui Li <yuanruili2000@gmail.com>
2019-09-14 17:24:24 -0400

    fixed update endpoint for profile

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require('path');
 var mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/hack', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/hack', {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
 const app = express();
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
@@ -13,13 +13,11 @@ app.use(bodyParser.json());
 
 // Serve the static files from the React app
 app.use(express.static(path.join(__dirname, 'client/build')));
-
 // MODELS
 var dosageSchema = new mongoose.Schema({
     time: String,
     amount: Number
-});
-
+}); 
 var usageHistorySchema = new mongoose.Schema({
     status: Boolean,
     updated_at: Date
@@ -36,7 +34,6 @@ var medicationSchema = new mongoose.Schema({
 
 var profileSchema = new mongoose.Schema({
     provider_id: {type: mongoose.Schema.Types.ObjectId, ref: "Providers"},
-    // provider_id: providerSchema,
     name: String,
     age: Number,
     email: String,
@@ -47,7 +44,6 @@ var providerSchema = new mongoose.Schema({
     name: String,
     email: String,
     phone: String,
-    // profiles: [{type: mongoose.Schema.Types.ObjectId, ref: "Profiles" }]
     profiles: [profileSchema]
 });
 
@@ -65,7 +61,7 @@ app.post('/api/provider-create', async (req, res) => {
     if (await Provider.findOne({email: req.body.email})) res.status(400).send();
     await Provider.insertOne({name: req.body.name, email: req.body.name, phone: req.body.phone})
         .then(provider => {
-            res.status(200).send(provider);
+            res.status(200).send(JSON.stringify(provider));
         })
         .catch(err => {
             res.status(400).send(err);
@@ -73,9 +69,9 @@ app.post('/api/provider-create', async (req, res) => {
 });
 
 app.get('api/get-provider/:id', async (req, res) => {
-    await Provider.findOne({_id: mongoose.Types.ObjectId(req.params.id)})
+    await Provider.findById(req.params.id)
         .then(provider => {
-            res.status(200).send(provider);
+            res.status(200).send(JSON.stringify(provider));
         })
         .catch(err => {
             res.status(400).send(err);
@@ -83,9 +79,9 @@ app.get('api/get-provider/:id', async (req, res) => {
 });
 
 app.delete('api/del-provider', async (req, res) => {
-    await Provider.deleteOne({_id: mongoose.Types.ObjectId(req.user._id)})
+    await Provider.findByIdAndRemove(req.user._id)
         .then(del => {
-            res.status(200).send(del)
+            res.status(200).send(JSON.stringify(del))
         })
         .catch(err => {
             res.status(400).send(err)
@@ -94,21 +90,20 @@ app.delete('api/del-provider', async (req, res) => {
 
 app.post('api/update-provider/:id', async (req, res) => { //adds new patient to the provider
    const provider = await Provider.updateOne({_id: req.user._id}, {name: req.body.name, email: req.body.email, phone: req.body.phone});
-   const profile = await Profile.findOne({_id: mongoose.Types.ObjectId(req.params.id)});
-   provider.profiles.push(profile);
-   profile.provider_id = provider;
    provider.markModified("profiles");
-   profile.markModified("provider_id");
-   await provider.save();
-   await profile.save();
-   if (provider && profile) res.status(200).send();
-   else res.status(400).send(err);
+   await provider.save()
+     .then(p => {
+       res.status(200).send(JSON.stringify(p));
+     })
+     .catch(err => {
+       res.status(400).send(err);
+     });
 });
 
 app.get('api/get-patients-provider/:id', async (req, res) => {
    await (Provider.findOne({_id: mongoose.Types.ObjectId(req.params.id)})).profiles
        .then(profiles => {
-           res.status(200).send(profiles);
+           res.status(200).send(JSON.stringify(profiles));
        })
        .catch(err => {
            res.status(400).send(err);
@@ -122,10 +117,13 @@ app.listen(port);
 
 // CREATE
 app.post('/api/profile', async (req, res) => {
-  let profile = new Profile(req.body.data);
+  let profile = new Profile(req.body.profile);
   await profile.save()
     .then((p) => {
-      res.status(200).send(p);
+      let provider = Provider.findById(req.body.providerId)
+      p.provider_id = provider;
+      provider.profiles.push(p);
+      res.status(200).send(JSON.stringify(p));
     })
     .catch(err => {
       res.status(400).send(err);
@@ -134,18 +132,23 @@ app.post('/api/profile', async (req, res) => {
 
 // PUT
 app.put('/api/profile/', async (req, res) => {
-  const profile = await Profile.findById(req.body.id);
-  profiles.medications.push(req.body.medication);
-  await profile.save();
-  if (profile) res.status(200).send(profile);
-  else res.status(400).send(err);
+  await Profile.findById(req.body.id)
+    .then((profile) => {
+      profile.medications.push(new Medication(req.body.medication));
+      profile.markModified('medications');
+      profile.save();
+      res.status(200).send(JSON.stringify(profile));
+    })
+    .catch(err => {
+      res.status(400).send(err);
+    });
 });
 
 // GET
 app.get('/api/profile/:id', async (req, res) => {
   await Profile.findById(req.params.id).lean().exec()
     .then((p) => {
-      res.status(200).send(p);
+      res.status(200).send(JSON.stringify(p));
     })
     .catch((e) => {
       res.status(400).send(err);
@@ -154,13 +157,17 @@ app.get('/api/profile/:id', async (req, res) => {
 
 // DELETE
 app.delete('/api/profile', async (req, res) => {
-  await Profile.deleteOne(res.body.query)
+  await Profile.findByIdAndRemove(req.body.id)
     .then(p => {
-      res.status(200).send(p);
+      res.status(200).send(JSON.stringify(p));
     })
     .catch(err => {
       res.status(400).send(err);
     });
 });
+
+// Medication endpoints
+
+// History endpoints
 
 console.log('App is listening on port ' + port);
