3b597a90e5c246aa47e564eeed44e31a1984a364
Rui Li <yuanruili2000@gmail.com>
2019-09-14 09:28:25 -0400

    added react-router package

--- client/src/App/pages/Home.js
+++ client/src/App/pages/Home.js
@@ -7,12 +7,6 @@ class Home extends Component {
     return (
     <div className="App">
       <h1>Project Home</h1>
-      {/* Link to List.js */}
-      <Link to={'./list'}>
-        <button variant="raised">
-            My List
-        </button>
-      </Link>
     </div>
     );
   }
