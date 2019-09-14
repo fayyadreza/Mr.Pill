e8f87fa25c01955c5f45574bc0a82fc450be89cf
Rui Li <yuanruili2000@gmail.com>
2019-09-14 10:48:32 -0400

    database connection

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const path = require('path');
+var mongoose = require('mongoose');
 
 const app = express();
+mongoose.connect('mongodb://localhost:27017/hack', {useNewUrlParser: true});
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+	console.log('connected');
+});
 
 // Serve the static files from the React app
 app.use(express.static(path.join(__dirname, 'client/build')));
 
-// An api endpoint that returns a short list of items
+// An api endpoint that returns a profile
 app.get('/api/profile', (req,res) => {
     res.json('test');
 });
