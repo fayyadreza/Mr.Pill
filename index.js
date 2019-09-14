e8afc05278a12424d40493acd9d43b03caae8f38
Rui Li <yuanruili2000@gmail.com>
2019-09-14 09:49:42 -0400

    removed unused import from Home.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.get('*', (req,res) =>{
     res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
 
-const port = 5000;
+const port = process.env.port || 5000;
 app.listen(port);
 
 console.log('App is listening on port ' + port);
