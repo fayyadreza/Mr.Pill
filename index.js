32b9f080de172553acf3cff8d4148d791752ad5b
Fayyad Reza <rezafayyad@gmail.com>
2019-09-14 13:47:32 -0400

    initial CRUD functions for providers

--- index.js
+++ index.js
@@ -14,40 +14,43 @@ var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 
 var providerSchema = new mongoose.Schema({
-  name: String,
-  email: String,
-  phone: String
+    name: String,
+    email: String,
+    phone: String,
+    // profiles: [{type: mongoose.Schema.Types.ObjectId, ref: "Profiles" }]
+    profiles: [profileSchema]
 });
 
 var dosageSchema = new mongoose.Schema({
-  time: String,
-  amount: Number
+    time: String,
+    amount: Number
 });
 
 var usageHistorySchema = new mongoose.Schema({
-  status: Boolean,
-  updated_at: Date
+    status: Boolean,
+    updated_at: Date
 });
 
 var medicationSchema = new mongoose.Schema({
-  status: Boolean,
-  name: String,
-  illness: String,
-  dosage: dosageSchema,
-  current_size: Number,
-  history: [usageHistorySchema]
+    status: Boolean,
+    name: String,
+    illness: String,
+    dosage: dosageSchema,
+    current_size: Number,
+    history: [usageHistorySchema]
 });
 
 var profileSchema = new mongoose.Schema({
-  provider_id: mongoose.Schema.Types.ObjectId,
-  name: String,
-  age: Number,
-  email: String,
-  medications: [medicationSchema]
+    // provider_id: {type: mongoose.Schema.Types.ObjectId, ref: "Providers"},
+    provider_id: [providerSchema],
+    name: String,
+    age: Number,
+    email: String,
+    medications: [medicationSchema]
 });
 
-var Provider = mongoose.model('Provider', providerSchema);
-var Profile = mongoose.model('Profile', profileSchema);
+const Provider = mongoose.model('Provider', providerSchema, "Providers");
+const Profile = mongoose.model('Profile', profileSchema, "Profiles");
 
 // Serve the static files from the React app
 app.use(express.static(path.join(__dirname, 'client/build')));
@@ -60,6 +63,60 @@ app.get('/api/profile', (req,res) => {
 // Handles any requests that don't match the ones above
 app.get('*', (req,res) =>{
     res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
+app.post('/api/provider-create', async (req, res) => {
+    if (await Provider.findOne({email: req.body.email})) res.status(400).send();
+    await Provider.insertOne({name: req.body.name, email: req.body.name, phone: req.body.phone})
+        .then(provider => {
+            res.status(200).send(provider);
+        })
+        .catch(err => {
+            res.status(400).send(err);
+        });
+});
+
+app.get('api/get-provider/:id', async (req, res) => {
+    await Provider.findOne({_id: mongoose.Types.ObjectId(req.params.id)})
+        .then(provider => {
+            res.status(200).send(provider);
+        })
+        .catch(err => {
+            res.status(400).send(err);
+        });
+});
+
+app.delete('api/del-provider', async (req, res) => {
+    await Provider.deleteOne({_id: mongoose.Types.ObjectId(req.user._id)})
+        .then(del => {
+            res.status(200).send(del)
+        })
+        .catch(err => {
+            res.status(400).send(err)
+        });
+});
+
+app.update('api/update-provider/:id', async (req, res) => { //adds new patient to the provider
+   const provider = await Provider.updateOne({_id: req.user._id}, {name: req.body.name, email: req.body.email, phone: req.body.phone});
+   const profile = await Profile.findOne({_id: mongoose.Types.ObjectId(req.params.id)});
+   provider.profiles.push(profile);
+   profile.provider_id = provider;
+   provider.markModified("profiles");
+   profile.markModified("provider_id");
+   await provider.save();
+   await profile.save();
+   if (provider && profile) res.status(200).send();
+   else res.status(400).send(err);
+});
+
+app.get('api/get-patients-provider/:id', async (req, res) => {
+   await (Provider.findOne({_id: mongoose.Types.ObjectId(req.params.id)})).profiles
+       .then(profiles => {
+           res.status(200).send(profiles);
+       })
+       .catch(err => {
+           res.status(400).send(err);
+       });
 });
 
 const port = process.env.PORT || 5000;
