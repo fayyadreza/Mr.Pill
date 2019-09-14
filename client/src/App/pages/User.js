968ae26d3fc24ccf709ca652c81e824dc1798afc
FlippadyFlap <46036342+FlippadyFlap@users.noreply.github.com>
2019-09-14 17:45:09 -0400

    Added link between users and profile

--- client/src/App/pages/User.js
+++ client/src/App/pages/User.js
@@ -9,6 +9,7 @@ import Sider from 'antd/lib/layout';
 import Content from 'antd/lib/layout';
 import Layout from 'antd/lib/layout';
 import Button from 'antd/lib/button';
+import Profile from './Profile';
 
 
 
@@ -17,62 +18,11 @@ class User extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            data_source: {},
-            selected_row_keys: []
+            data_source: {}
         };
 
         //Sample data source
-        this.data_source = [{
-            key: '0',
-            name: 'Nitroglycerin',
-            condition: 'Angina',
-            dosage: '1 tablet',
-            amount_remaining: 20,
-            time: 'Morning'
-        },
-        {
-            key: '1',
-            name: 'Penicillin',
-            condition: 'Infection',
-            dosage: '1 tablet',
-            amount_remaining: 14,
-            time: 'Morning'
-        }, {
-            key: '1',
-            name: 'Penicillin',
-            condition: 'Infection',
-            dosage: '1 tablet',
-            amount_remaining: 14,
-            time: 'Morning'
-        }, {
-            key: '1',
-            name: 'Penicillin',
-            condition: 'Infection',
-            dosage: '1 tablet',
-            amount_remaining: 14,
-            time: 'Morning'
-        }, {
-            key: '1',
-            name: 'Penicillin',
-            condition: 'Infection',
-            dosage: '1 tablet',
-            amount_remaining: 14,
-            time: 'Morning'
-        }, {
-            key: '1',
-            name: 'Penicillin',
-            condition: 'Infection',
-            dosage: '1 tablet',
-            amount_remaining: 14,
-            time: 'Morning'
-        }, {
-            key: '1',
-            name: 'Penicillin',
-            condition: 'Infection',
-            dosage: '1 tablet',
-            amount_remaining: 14,
-            time: 'Morning'
-        }];
+        this.data_source = [{ name: "poopie", age: 22, id: 12345 }];
 
     }
 
@@ -82,24 +32,17 @@ class User extends Component {
             <body>
                 <Layout>
                     <h1>Welcome </h1>
-                    <h2>Current Medications</h2>
+                    <h2><u>Your Patients</u></h2>
                     <Sider />
                     <Content>
                         <div classname='meds-table'>
-                            <Table dataSource={this.data_source} size="small">
-                                <Column title="Name" dataIndex="name" key="name" />
-                                <Column title="Condition" dataIndex="condition" key="condition" />
-                                <Column title="Dosage" dataIndex="dosage" key="dosage" />
-                                <Column title="Time" dataIndex="time" key="time" />
-                                <Column title="Amount Remaining" dataIndex="amount_remaining" key="amount_remaining" />
-
+                            <Table dataSource={this.data_source} size="small" rowKey="uid">
+                                <Column title="Name" dataIndex="name" />
+                                <Column title="Age" dataIndex="age" />
+                                <Column title="Id" dataIndex="id" />
                                 <Column
                                     title=""
-                                    key="remove" render={() => <Button type='danger'>Remove</Button>} />
-
-                                <Column
-                                    title=""
-                                    key="update" render={() => <Button type='primary'>Decrement</Button>} />
+                                    render={(id, name) => <Button type='primary'><Link to={{ pathname: '/profile', state: { uid: id, name: name }, Component: { Profile } }} > View Profile </Link></Button>} />
                             </Table>
                         </div>
                     </Content>
