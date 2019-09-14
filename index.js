d7d9ae8ffff0f34af7738ac835d6d103ed8a3e06
Rui Li <yuanruili2000@gmail.com>
2019-09-14 13:51:45 -0400

    endpoints for profile

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 const express = require('express');
 const path = require('path');
 var mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const app = express();
 mongoose.connect('mongodb://localhost:27017/hack', {useNewUrlParser: true, useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Serve the static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+////////////////////////////////////////////////////
+// MODELS
 var providerSchema = new mongoose.Schema({
   name: String,
   email: String,
-  phone: String
-});
+  phone: String });
 
 var dosageSchema = new mongoose.Schema({
   time: String,
@@ -43,20 +50,45 @@ var profileSchema = new mongoose.Schema({
 var Provider = mongoose.model('Provider', providerSchema);
 var Profile = mongoose.model('Profile', profileSchema);
 
-// Serve the static files from the React app
-app.use(express.static(path.join(__dirname, 'client/build')));
-
-// An api endpoint that returns a profile
-app.get('/api/profile', (req,res) => {
-    res.json('test');
-});
-
 // Handles any requests that don't match the ones above
 app.get('*', (req,res) =>{
-    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
 
 const port = process.env.PORT || 5000;
 app.listen(port);
+
+// Profile endpoints
+
+// CREATE
+app.post('/api/profile', (req, res) => {
+  console.log(req);
+  let profile = new Profile(req.body.data);
+  profile.save(function (err, p) {
+    if (err) return res.json(JSON.stringify(err));
+    res.json(p);
+  });
+});
+
+// GET
+app.get('/api/profile', (req, res) => {
+  console.log(req);
+  Profile.find().lean().exec(req.params.query, function (err, p) {
+    if (err) return res.json(JSON.stringify(err));
+    res.json(JSON.stringify(p));
+  });
+});
+
+// PUT
+
+// DELETE
+app.delete('/api/profile', (req, res) => {
+  Profile.deleteOne(res.body.query, function (err, p) {
+    if (err) return res.status(400).end();
+    res.status(200).end();
+  });
+});
+
+// Provider endpoints
 
 console.log('App is listening on port ' + port);
