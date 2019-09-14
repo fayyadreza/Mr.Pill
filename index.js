e153359160c722714ca0bec317bc4137db1a9b4b
Rui Li <yuanruili2000@gmail.com>
2019-09-14 09:42:05 -0400

    changed port

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.get('*', (req,res) =>{
     res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
 
-const port = process.env.PORT || 5000;
+const port = 5000;
 app.listen(port);
 
 console.log('App is listening on port ' + port);
