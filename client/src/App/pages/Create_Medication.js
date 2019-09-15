f3597636e4f734f991c68f4e9792c6baf7c772cf
FlippadyFlap <46036342+FlippadyFlap@users.noreply.github.com>
2019-09-14 23:07:18 -0400

    Update Create_Medication.js

--- client/src/App/pages/Create_Medication.js
+++ client/src/App/pages/Create_Medication.js
@@ -7,8 +7,32 @@ class Create_Medication extends Component {
     constructor(props) {
         super(props);
         state = {
+            ModalText: 'Please Enter The Information For The New Medication',
+            visible: false,
+            confirmLoading: false,
             id: this.props.id
+        };
+
+        showModal = () => {
+            this.setState({
+                visible: true,
+            });
+        };
+
+        handleCreate = () => {
+            this.setState({
+                ModalText: 'The modal will be closed after two seconds',
+                confirmLoading: true,
+            });
+            setTimeout(() => {
+                this.setState({
+                    visible: false,
+                    confirmLoading: false,
+                });
+            }, 2000);
         }
+
+
     }
 
     render() {
@@ -16,7 +40,7 @@ class Create_Medication extends Component {
             <h1>Enter Information For New Medication</h1>
             <span>
                 <Form layout="inline" onSubmit={}>
-                    <Input></Input>
+
                 </Form>
             </span>
 
