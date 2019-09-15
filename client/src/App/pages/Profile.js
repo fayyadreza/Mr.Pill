9c8e8600fb22f5be45f3695297d61c1c0e76656a
Jeff Lai <jefflai333@gmail.com>
2019-09-15 06:30:55 -0400

    add phone number to schema

--- client/src/App/pages/Profile.js
+++ client/src/App/pages/Profile.js
@@ -1,109 +1,107 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import { checkPropTypes } from 'prop-types';
-import CollectionsPage from './Modal_Form.js';
-import 'antd/dist/antd.css';
-import './main.css';
-import Table from 'antd/lib/table/Table';
-import Column from 'antd/lib/table/Column';
-import Sider from 'antd/lib/layout';
-import Content from 'antd/lib/layout';
-import Layout from 'antd/lib/layout';
-import Button from 'antd/lib/button';
-import { Row, Col } from 'antd';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { checkPropTypes } from "prop-types";
+import CollectionsPage from "./Modal_Form.js";
+import "antd/dist/antd.css";
+import "./main.css";
+import Table from "antd/lib/table/Table";
+import Column from "antd/lib/table/Column";
+import Sider from "antd/lib/layout";
+import Content from "antd/lib/layout";
+import Layout from "antd/lib/layout";
+import Button from "antd/lib/button";
+import { Row, Col } from "antd";
 
 class Profile extends Component {
-    constructor(props) {
-        super(props);
-        let user_info = props.location.state;
-        console.log(user_info);
-        this.state = {
-            name: user_info.name,
-            id: user_info._id,
-            data_source: user_info.medications
-        };
+  constructor(props) {
+    super(props);
+    let user_info = props.location.state;
+    console.log(user_info);
+    this.state = {
+      name: user_info.name,
+      id: user_info._id,
+      phone: user_info.phone,
+      data_source: user_info.medications
+    };
 
-        fetch(
-            "https://hackthenorth2019.herokuapp.com/api/profile/" + this.state.id
-        ).then(response => {
-              console.log('response', response);
-              response.json().then(
-                  data => {
-                    console.log(data);
-                    this.setState({ data_source: data.medications });
-                  }
-              )
-           }
-        );
-        var handleDecrement = function (id) {
-            fetch(
-                "https://hackthenorth2019.herokuapp.com/api/decrement-dosage" + id
-            ).then(response =>
-                response.json().then(data => {
-                    let id_list = data.data_source.id;
-                    id_list[id_list.find(id)] = id - 1;
-                    this.state.data_source.id = id_list;
-                }));
-        }
+    fetch(
+      "https://hackthenorth2019.herokuapp.com/api/profile/" + this.state.id
+    ).then(response => {
+      console.log("response", response);
+      response.json().then(data => {
+        console.log(data);
+        this.setState({ data_source: data.medications });
+      });
+    });
+    var handleDecrement = function(id) {
+      fetch(
+        "https://hackthenorth2019.herokuapp.com/api/decrement-dosage" + id
+      ).then(response =>
+        response.json().then(data => {
+          let id_list = data.data_source.id;
+          id_list[id_list.find(id)] = id - 1;
+          this.state.data_source.id = id_list;
+        })
+      );
+    };
+  }
+  fetchTableProps(id) {
+    fetch("https://hackthenorth2019.herokuapp.com/api/profile/" + id).then(
+      response => {
+        console.log("response", response);
+        response.json().then(data => {
+          if (data) {
+            this.setState({ data_source: data.medications });
+          }
+        });
+      }
+    );
+  }
 
-    }
-    fetchTableProps (id) {
-        fetch(
-            "https://hackthenorth2019.herokuapp.com/api/profile/" + id
-        ).then(response => {
-              console.log('response', response);
-              response.json().then(
-                  data => {
-                      if (data) {
-                          this.setState({ data_source: data.medications });
-                      }
-                  }
-              )
-           }
-        );
-    }
+  render() {
+    return (
+      <Row type="flex" justify="center">
+        <Col span={22}>
+          <h1>This is the profile of {this.state.name} </h1>
+          <h2>Current Medications</h2>
 
+          <div classname="meds-table">
+            <Table
+              dataSource={this.state.data_source}
+              size="small"
+              rowKey="_id"
+            >
+              <Column title="Name" dataIndex="name" key="name" />
+              <Column title="Condition" dataIndex="condition" key="condition" />
+              <Column title="Dosage" dataIndex="dosage" />
+              <Column title="Time" dataIndex="time" key="time" />
+              <Column
+                title="Amount Remaining"
+                dataIndex="amount_remaining"
+                key="amount_remaining"
+              />
 
-    render() {
-        return (
-                <Row type="flex" justify="center">
-                    <Col span={22}>
-                        <h1>This is the profile of {this.state.name} </h1>
-                        <h2>Current Medications</h2>
-
-                            <div classname="meds-table">
-                                <Table dataSource={this.state.data_source} size="small" rowKey="_id">
-                                    <Column title="Name" dataIndex="name" key="name" />
-                                    <Column
-                                        title="Condition"
-                                        dataIndex="condition"
-                                        key="condition"
-                                    />
-                                    <Column title="Dosage" dataIndex="dosage" />
-                                    <Column title="Time" dataIndex="time" key="time" />
-                                    <Column
-                                        title="Amount Remaining"
-                                        dataIndex="amount_remaining"
-                                        key="amount_remaining"
-                                    />
-
-                                    {/* <Column
+              {/* <Column
                                 title=""
                                 key="remove"
                                 render={() => <Button type="danger">Remove</Button>}
                             /> */}
 
-                                    <Column
-                                        title=""
-                                        key="update"
-                                        render={() => <Button type="primary" onClick={this.fetchTableProps()}>Update</Button>}
-                                    />
-                                </Table>
-                                <CollectionsPage profile={ this.state.id } />
-                            </div>
-                    </Col>
-                </Row>
-        );
-    }
+              <Column
+                title=""
+                key="update"
+                render={() => (
+                  <Button type="primary" onClick={this.fetchTableProps()}>
+                    Update
+                  </Button>
+                )}
+              />
+            </Table>
+            <CollectionsPage profile={this.state.id} />
+          </div>
+        </Col>
+      </Row>
+    );
+  }
 }
 export default Profile;
--- index.js
+++ index.js
@@ -4,9 +4,9 @@ var mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/hack", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false
 });
 const app = express();
 var db = mongoose.connection;
@@ -17,45 +17,46 @@ app.use(bodyParser.json());
 
 // MODELS
 var dosageSchema = new mongoose.Schema({
-    time: String,
-    amount: Number
+  time: String,
+  amount: Number
 });
 
 var usageHistorySchema = new mongoose.Schema({
-    status: Boolean,
-    updated_at: Date
+  status: Boolean,
+  updated_at: Date
 });
 
 var medicationSchema = new mongoose.Schema({
-    status: Boolean,
-    name: String,
-    illness: String,
-    dosage: dosageSchema,
-    current_size: Number,
-    history: [usageHistorySchema]
+  status: Boolean,
+  name: String,
+  illness: String,
+  dosage: dosageSchema,
+  current_size: Number,
+  history: [usageHistorySchema]
 });
 
 var profileSchema = new mongoose.Schema({
-    provider_id: { type: mongoose.Schema.Types.ObjectId, ref: "Providers" },
-    name: String,
-    age: Number,
-    email: String,
-    medications: [medicationSchema]
+  provider_id: { type: mongoose.Schema.Types.ObjectId, ref: "Providers" },
+  name: String,
+  age: Number,
+  email: String,
+  phone: String,
+  medications: [medicationSchema]
 });
 
 var providerSchema = new mongoose.Schema({
-    name: String,
-    email: String,
-    phone: String,
-    profiles: [profileSchema]
+  name: String,
+  email: String,
+  phone: String,
+  profiles: [profileSchema]
 });
 
 const Provider = mongoose.model("Provider", providerSchema, "Providers");
 const Profile = mongoose.model("Profile", profileSchema, "Profiles");
 const Medication = mongoose.model(
-    "Medication",
-    medicationSchema,
-    "Medications"
+  "Medication",
+  medicationSchema,
+  "Medications"
 );
 const History = mongoose.model("History", usageHistorySchema, "Histories");
 const Dosage = mongoose.model("Dosage", dosageSchema, "Dosages");
@@ -64,64 +65,64 @@ const Dosage = mongoose.model("Dosage", dosageSchema, "Dosages");
 app.use(express.static(path.join(__dirname, "client/build")));
 
 app.post("/api/provider-create", async (req, res) => {
-    let provider = new Provider(req.body.data);
-    await provider
-        .save()
-        .then(p => {
-            res.status(200).send(p);
-        })
-        .catch(err => {
-            res.status(400).send(err);
-        });
+  let provider = new Provider(req.body.data);
+  await provider
+    .save()
+    .then(p => {
+      res.status(200).send(p);
+    })
+    .catch(err => {
+      res.status(400).send(err);
+    });
 });
 
 app.get("/api/get-provider/:id", async (req, res) => {
-    await Provider.findById(req.params.id)
-        .lean()
-        .exec()
-        .then(provider => {
-            res.status(200).send(provider);
-        })
-        .catch(err => {
-            res.status(400).send(err);
-        });
+  await Provider.findById(req.params.id)
+    .lean()
+    .exec()
+    .then(provider => {
+      res.status(200).send(provider);
+    })
+    .catch(err => {
+      res.status(400).send(err);
+    });
 });
 
 app.delete("/api/del-provider", async (req, res) => {
-    await Provider.findByIdAndRemove(req.user._id)
-        .then(del => {
-            res.status(200).send(del);
-        })
-        .catch(err => {
-            res.status(400).send(err);
-        });
+  await Provider.findByIdAndRemove(req.user._id)
+    .then(del => {
+      res.status(200).send(del);
+    })
+    .catch(err => {
+      res.status(400).send(err);
+    });
 });
 
 app.post("/api/update-provider/:id", async (req, res) => {
-    //adds new patient to the provider
-    const provider = await Provider.updateOne(
-        { _id: req.user._id },
-        { name: req.body.name, email: req.body.email, phone: req.body.phone }
-    );
-    provider.markModified("profiles");
-    await provider
-        .save()
-        .then(p => {
-            res.status(200).send(p);
-        })
-        .catch(err => {
-            res.status(400).send(err);
-        });
+  //adds new patient to the provider
+  const provider = await Provider.updateOne(
+    { _id: req.user._id },
+    { name: req.body.name, email: req.body.email, phone: req.body.phone }
+  );
+  provider.markModified("profiles");
+  await provider
+    .save()
+    .then(p => {
+      res.status(200).send(p);
+    })
+    .catch(err => {
+      res.status(400).send(err);
+    });
 });
 
 app.get("/api/get-patients-provider/:id", async (req, res) => {
-    await Provider.findOne({ _id: mongoose.Types.ObjectId(req.params.id) })
-        .profiles.then(profiles => {
-            res.status(200).send(profiles);
-        })
-        .catch(err => {
-            res.status(400).send(err);
-        });
+  await Provider.findOne({ _id: mongoose.Types.ObjectId(req.params.id) })
+    .profiles.then(profiles => {
+      res.status(200).send(profiles);
+    })
+    .catch(err => {
+      res.status(400).send(err);
+    });
 });
 
 const port = process.env.PORT || 5000;
@@ -131,146 +132,146 @@ app.listen(port);
 
 // CREATE
 app.post("/api/profile", async (req, res) => {
-    let profile = new Profile(req.body.profile);
-    await profile.save();
-    await Provider.findById(req.body.profile.provider_id)
-        .then(p => {
-            p.profiles.push(profile);
-            p.markModified("profiles");
-            p.save();
-            res.status(200).end(profile);
-        })
-        .catch(err => {
-            res.status(400).send(err);
-        });
+  let profile = new Profile(req.body.profile);
+  await profile.save();
+  await Provider.findById(req.body.profile.provider_id)
+    .then(p => {
+      p.profiles.push(profile);
+      p.markModified("profiles");
+      p.save();
+      res.status(200).end(profile);
+    })
+    .catch(err => {
+      res.status(400).send(err);
+    });
 });
 
 // PUT
 app.put("/api/profile", async (req, res) => {
-    let med = new Medication(req.body.medication);
-    await med.save();
-    await Profile.findById(req.body.id)
-        .then(profile => {
-            profile.medications.push(med);
-            profile.markModified("medications");
-            profile.save();
-            res.status(200).send(profile);
-        })
-        .catch(err => {
-            res.status(400).send(err);
-        });
+  let med = new Medication(req.body.medication);
+  await med.save();
+  await Profile.findById(req.body.id)
+    .then(profile => {
+      profile.medications.push(med);
+      profile.markModified("medications");
+      profile.save();
+      res.status(200).send(profile);
+    })
+    .catch(err => {
+      res.status(400).send(err);
+    });
 });
 
 // GET
 app.get("/api/profile/:id", async (req, res) => {
-    await Profile.findById(req.params.id)
-        .lean()
-        .exec()
-        .then(p => {
-            res.status(200).send(p);
-        })
-        .catch(err => {
-            res.status(400).send(err);
-        });
+  await Profile.findById(req.params.id)
+    .lean()
+    .exec()
+    .then(p => {
+      res.status(200).send(p);
+    })
+    .catch(err => {
+      res.status(400).send(err);
+    });
 });
 
 app.get("/api/get-profile-by-email/:email", async (req, res) => {
-    await Profile.findOne({ email: req.params.email })
-        .lean()
-        .exec()
-        .then(p => {
-            res.status(200).send(p);
-        })
-        .catch(err => {
-            res.status(400).send(err);
-        });
+  await Profile.findOne({ email: req.params.email })
+    .lean()
+    .exec()
+    .then(p => {
+      res.status(200).send(p);
+    })
+    .catch(err => {
+      res.status(400).send(err);
+    });
 });
 
 app.get("/api/get-profile-by-phone/:phone", async (req, res) => {
-    await Profile.findOne({ phone: req.params.phone })
-        .lean()
-        .exec()
-        .then(p => {
-            res.status(200).send(p);
-        })
-        .catch(err => {
-            res.status(400).send(err);
-        });
+  await Profile.findOne({ phone: req.params.phone })
+    .lean()
+    .exec()
+    .then(p => {
+      res.status(200).send(p);
+    })
+    .catch(err => {
+      res.status(400).send(err);
+    });
 });
 
 // DELETE
 app.delete("/api/profile", async (req, res) => {
-    await Profile.findByIdAndRemove(req.body.id)
-        .then(p => {
-            res.status(200).send(p);
-        })
-        .catch(err => {
-            res.status(400).send(err);
-        });
+  await Profile.findByIdAndRemove(req.body.id)
+    .then(p => {
+      res.status(200).send(p);
+    })
+    .catch(err => {
+      res.status(400).send(err);
+    });
 });
 
 // Medication endpoints
 
 app.put("/api/decrement-dosage", async (req, res) => {
-    console.log(Date.now + " 203 ");
-    console.log(req);
-    const profile = await Profile.findById(req.body.user_id);
-    console.log(Date.now + " 204 " + profile);
-    await Medication.findById(req.body.medicationId)
-        .then(medication => {
-            const amount = req.body.amount;
-            console.log(Date.now + " 207 " + amount);
-            if (medication.current_size >= medication.dosage.amount) {
-                console.log(Date.now + " 208 " + medication);
-                for (const med of profile.medications) {
-                    console.log(Date.now + " " + med);
-                    if (med._id.equals(req.body.medicationId)) {
-                        med.current_size -= amount;
-                        break;
-                    }
-                }
-                // (profile.medications.filter(medId => !medId.equals(med._id))).current_size -= amount;//this should work
-                medication.current_size -= amount;
-            } else {
-                medication.status = true;
-            }
-            profile.save();
-            medication.markModified("current_size");
-            medication.save();
-            res.status(200).send(medication);
-        })
-        .catch(err => {
-            res.status(400).send(err);
-        });
+  console.log(Date.now + " 203 ");
+  console.log(req);
+  const profile = await Profile.findById(req.body.user_id);
+  console.log(Date.now + " 204 " + profile);
+  await Medication.findById(req.body.medicationId)
+    .then(medication => {
+      const amount = req.body.amount;
+      console.log(Date.now + " 207 " + amount);
+      if (medication.current_size >= medication.dosage.amount) {
+        console.log(Date.now + " 208 " + medication);
+        for (const med of profile.medications) {
+          console.log(Date.now + " " + med);
+          if (med._id.equals(req.body.medicationId)) {
+            med.current_size -= amount;
+            break;
+          }
+        }
+        // (profile.medications.filter(medId => !medId.equals(med._id))).current_size -= amount;//this should work
+        medication.current_size -= amount;
+      } else {
+        medication.status = true;
+      }
+      profile.save();
+      medication.markModified("current_size");
+      medication.save();
+      res.status(200).send(medication);
+    })
+    .catch(err => {
+      res.status(400).send(err);
+    });
 });
 
 app.delete("/api/delete-med", async (req, res) => {
-    const med = await Medication.findById(req.body.medicationId);
-    const dosage = med.dosage;
-    const profile = await Profile.findById(med.provider_id);
-    profile.medications = profile.medications.filter(
-        medId => !medId.equals(med._id)
-    ); //this should work
-    await Dosage.deleteOne({ _id: dosage._id }).save();
-    await Medication.deleteOne({ _id: med._id })
-        .save()
-        .then(med => {
-            res.status(200).send(med);
-        })
-        .catch(err => {
-            res.status(400).send(err);
-        });
+  const med = await Medication.findById(req.body.medicationId);
+  const dosage = med.dosage;
+  const profile = await Profile.findById(med.provider_id);
+  profile.medications = profile.medications.filter(
+    medId => !medId.equals(med._id)
+  ); //this should work
+  await Dosage.deleteOne({ _id: dosage._id }).save();
+  await Medication.deleteOne({ _id: med._id })
+    .save()
+    .then(med => {
+      res.status(200).send(med);
+    })
+    .catch(err => {
+      res.status(400).send(err);
+    });
 });
 
 // Reminders that Google Home will provide to the patient to refill prescription
 async function reminder(medicationId) {
-    const med = await Medication.findOne({
-        _id: mongoose.Types.ObjectId(medicationId)
-    });
-    if (med.dosage === 5)
-        return "You now have 5 dosages left. Prepare to refill soon!";
-    else if (med.dosage === 0)
-        return "You now have 0 dosages left. Please refill your prescription or delete the medication from your account";
+  const med = await Medication.findOne({
+    _id: mongoose.Types.ObjectId(medicationId)
+  });
+  if (med.dosage === 5)
+    return "You now have 5 dosages left. Prepare to refill soon!";
+  else if (med.dosage === 0)
+    return "You now have 0 dosages left. Please refill your prescription or delete the medication from your account";
 }
 
 console.log("App is listening on port " + port);
